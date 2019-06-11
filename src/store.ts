c7a7ac553bded897d3eeca221fa739055946cd19
YTTTTY <13112688275@163.com>
2019-06-11 11:21:26 +0800

    footer

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    count: 1,
   },
   mutations: {
-
+    increment(state, data) {
+      state.count = data;
+    },
   },
   actions: {
 
